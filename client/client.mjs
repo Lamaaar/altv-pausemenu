e0dbd791a549e763284367db59ca01107cb3b08b
Lamar <64546635+Lamaaar@users.noreply.github.com>
2021-07-21 23:46:32 +0200

    Publishing
    Publishing the source code

---
+++ client/client.mjs
@@ -0,0 +1,16 @@
+alt.addGxtText("PM_SCR_INF", "Információk");
+alt.addGxtText("PM_SCR_SET", "Beállítások");
+alt.addGxtText("PM_SCR_MAP", "Térkép");
+alt.addGxtText("PM_PANE_FEE", "Értesítések");
+alt.addGxtText("PM_PANE_HLP", "Segítség");
+alt.addGxtText("IB_ZOOM", "Nagyítás");
+alt.addGxtText("IB_WAYPOINT", "Kijelölés");
+alt.addGxtText("MO_BLI", "Ikon");
+alt.addGxtText("IB_LEGEND", "Jelmagyarázat");
+alt.addGxtText("IB_HIDEMENU", "Menü elrejtése");
+alt.addGxtText("IB_SHOWMENU", "Menü megjelenítése");
+alt.addGxtText("IB_OPTIONS", "Opciók");
+alt.addGxtText("IB_SELECT", "Kiválasztás");
+alt.addGxtText("IB_BACK", "Vissza");
+alt.addGxtText("HUD_INPUT1C", "Böngészés");
+alt.addGxtText("IB_POI", "Mentett hely");
